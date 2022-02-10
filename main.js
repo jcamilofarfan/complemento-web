e7e09d3e009821d599c21300b21fd1beb0cf375f
jcamilofarfan <farfan1976@hotmail.com>
2022-02-10 13:04:42 -0500

    add count courses

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ const header = document.querySelector("div.profile-main-container div.resume");
 const titleHeader = document.querySelector("div.profile-main-container div.resume h3");
 const sectionCourses = document.querySelector("div div.courses-main-container");
 const sectionJobs = document.querySelector("div div.jobs-main-container");
+const countCourses = document.querySelector("div span.count-courses");
 async function obtenerData(){
     const datos = await fetch("https://cache1.phantombooster.com/pzuhC3LNq4M/tP6uZr2e4K1CGAjZ7pxw2A/result.json");
     const datosJson = datos.json();
     return datosJson;
 }
+
 function populateHeader(jsonObj) {
     const general = jsonObj[0]["general"];
     titleHeader.textContent = "Hola, Mi nombre es " + general.fullName;
@@ -14,9 +16,17 @@ function populateHeader(jsonObj) {
     const myresume = document.createElement("p");
     myresume.textContent = general.description;
     header.appendChild(myresume);
+
 }
 function showlicences(jsonObj) {
     const licences = jsonObj[0]["licences"];
+    const countLicenses = licences.length;
+    const mySpan = document.createElement("a");
+    mySpan.textContent = "Platzi";
+    mySpan.href = "https://platzi.com/p/jcamilofarfan/";
+    mySpan.target = "_blank";
+    countCourses.innerHTML = 'Se muestran  '+ countLicenses + ' Cursos para mas puede visitar mi perfil de '+mySpan.outerHTML;
+
     for (let i = 0; i < licences.length; i++) {
         const myArticle = document.createElement("article");
         myArticle.className += "courses-container";
@@ -45,7 +55,7 @@ function showJobs(jsonObj){
         if (jobs[i].jobTitle===null) {
             myH4.textContent = jobs[i].companyName;
         }else{
-            myH4.textContent = jobs[i].jobTitle + " en " + jobs[i].companyName;    
+            myH4.textContent = jobs[i].jobTitle + " en " + jobs[i].companyName;
         }
         fecha.textContent = jobs[i].dateRange;
         const descriptionText = jobs[i].description;
@@ -60,6 +70,7 @@ function showJobs(jsonObj){
 
     }
 }
+
 obtenerData().then(populateHeader);
 obtenerData().then(showlicences);
 obtenerData().then(showJobs);
